b801177ca5eb00b5bbb2a57081d7039e5e88e559
Wagner <mitobrmatanob@gmail.com>
2026-05-12 16:55:48 -0300

    Add Google Tag Manager GTM-M3PRBWKQ

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Fraunces, Inter, JetBrains_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -48,7 +49,30 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${inter.variable} ${jetBrainsMono.variable} ${fraunces.variable}`}
     >
-      <body suppressHydrationWarning>{children}</body>
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M3PRBWKQ');`
+          }}
+        />
+      </head>
+      <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M3PRBWKQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
